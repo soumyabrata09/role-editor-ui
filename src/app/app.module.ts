53ca7dc9aebb733cfe809d6e9ba6b5379ebad3d6
Soumyabrata Sen <soumyabratasen@hotmail.com>
2024-03-31 03:56:59 +0530

    Role and User component created; role html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     declarations: [
         AppComponent
     ],
     imports: [
         BrowserModule,
-        //   AppRoutingModule,
+        AppRoutingModule,
         HttpClientModule,
         FormsModule
     ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "users", component: UserComponent
+    },
+    {
+        path: "roles", component: RoleComponent
+    },
+    {
+        path: "", redirectTo: "/users", pathMatch: "full"
+    }
+];
